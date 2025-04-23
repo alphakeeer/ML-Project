f926846ccec396b6d43924028e2c32762eddd350
sato <sato@nana.com>
2025-04-23 19:39:01 +0800

    dataloader新增处理

--- data_loader.py
+++ data_loader.py
@@ -24,10 +24,22 @@ native-country: United-States, Cambodia, England, Puerto-Rico, Canada, Germany, 
 
 >50K, <=50K.
 
+workclass: 8
+education: 16
+marital-status: 7
+occupation: 14
+relationship: 6
+race: 5
+sex: 2
+native-country: 41
 '''
 
 
 import pandas as pd
+import numpy as np
+from typing import Tuple
+from sklearn.preprocessing import StandardScaler
+
 class DataLoader:
     def __init__(self):
         pass
@@ -40,34 +52,64 @@ class DataLoader:
         output_file_path: str, 输出文件路径, 如果不为None, 则将数据转为csv保存到该路径
         '''
         df = pd.read_csv(file_path, header=None)
-        df.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status',
-                      'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',
-                      'hours-per-week', 'native-country', 'income']
+        if not file_path.endswith('.csv'):
+            df.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status',
+                          'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',
+                          'hours-per-week', 'native-country', 'income']
         if output_file_path:
             df.to_csv(output_file_path, index=False)
         return df
-    
-    def preprocess_data(self, df: pd.DataFrame) -> pd.DataFrame:
+
+    def save_data(self, data, file_path: str):
+        '''
+        保存数据到csv文件
+        data: pd.DataFrame 或 numpy.ndarray, 数据
+        file_path: str, 文件路径
+        '''
+        if isinstance(data, np.ndarray):
+            data = pd.DataFrame(data)
+        data.to_csv(file_path, index=False)
+        return data
+
+    def preprocess_data_svm(self, df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
         '''
         预处理数据
         1. 处理缺失值
         2. 处理非数值
         3. 进一步处理
         '''
-        # 处理缺失值——直接删
+
+        # 处理缺失值->直接删
         df.replace(' ?', pd.NA, inplace=True)
         df.dropna(inplace=True)
 
         # 处理非数值
-        # 
-        categorical_cols = df.select_dtypes(include=['object']).columns
-        for col in categorical_cols:
-            df[col] = df[col].astype('category').cat.codes
+        low_card = ['sex', 'relationship', 'race', 'marital-status']
+        high_card = ['workclass', 'education', 'occupation', 'native-country']
 
-        return df
+        # 低基数->onehont
+        df = pd.get_dummies(df, columns=low_card, drop_first=True)
+
+        # 高基数->频率编码
+        for col in high_card:
+            freq = df[col].value_counts(normalize=True)
+            df[col] = df[col].map(freq)
+        
+        # 分离标签
+        y = df['income'].apply(lambda x: 1 if x == '>50K' else 0)
+        X = df.drop('income', axis=1)
+        
+        # 标准化
+        scaler = StandardScaler()
+        X_scaler = scaler.fit_transform(X)
+        X_df=pd.DataFrame(X_scaler, columns=X.columns,index=X.index)
+
+        return X_df, y
 
 
 if __name__ == "__main__":
     data_loader = DataLoader()
-    df = data_loader.load_data('data/adult.csv')
+    df = data_loader.load_data('raw/adult.data', 'data/adult.csv')
+    df,y = data_loader.preprocess_data_svm(df)
+    df=data_loader.save_data(df, 'data/adult_preprocessed.csv')
     print(df.head())
